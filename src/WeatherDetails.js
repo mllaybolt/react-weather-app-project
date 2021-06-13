3c4dfbc1abff8b3e2ce2fd47ad8385bf78e0843e
mllaybolt <81402055+mllaybolt@users.noreply.github.com>
2021-06-13 14:58:01 -0500

    installed axios, wrote date formatting, added OpenWeather API url

--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -1,45 +1,67 @@
 import React, { useState } from "react";
+import axios from "axios";
+import TempConversion from "./TempConversion";
+import FormattedDate from "./FormattedDate";
 
 export default function WeatherDetails(props) {
-    const [weatherData, setWeatherData] - useState({ ready: false});
-    const [city, setCity] = useState(props.defaultCity);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-    function search() {
-        let apiUrl = //need to install axios and populate OpenWeather API data and link
-        axios.get(apiUrl).then(handleResponse);
-    }
+  function search() {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=66e11cc33b0837aff2265fadb13ac0ad&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        search();
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-    function handleCityChange(event) {
-        setCity(event.target.value);
-}
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
-function handleResponse(response) {
+  function handleResponse(response) {
     setWeatherData({
-        ready: true,
-        temperature: response.data.main.temp,
-        description: response.data.weather[0].description,
-        city: response.data.name,
-        date: new Date(response.data.dt * 1000),
-        icon: // need to link to OpenWeather icons
+      ready: true,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
-}
+  }
 
-if(weatherData.ready) {
+  if (weatherData.ready) {
     return (
-        <div className="Temp">
-            <form id="search-form" onSubmit={handleSubmit}>
-                <input type="search" placeholder="Search for a city" id="city-input" onChange={handleCityChange}/>
-                <input type="submit" id="search-button" value="Search" />
-                <button id="current-temperature-button">current</button>
-            </form>
-        </div>
-    )
-}
+      <div className="Temp">
+        <form id="search-form" onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Search for a city"
+            id="city-input"
+            onChange={handleCityChange}
+          />
+          <input type="submit" id="search-button" value="Search" />
+          <button id="current-temperature-button">current</button>
+        </form>
 
+        <h3 id="date">
+          <FormattedDate date={weatherData.date} />
+        </h3>
+        <h1 id="current-location">{weatherData.city}</h1>
+        <TempConversion celsius={weatherData.temperature} />
 
+        <h3 id="weather-conditions">{weatherData.description}</h3>
+        <img
+          src={weatherData.icon}
+          className="main-icon"
+          alt="current-weather"
+        />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading information...";
+  }
 }
